10e1b20825f2d5a4d76efb7c846cde964ebcbf09
Charles Liu <charles.liu0309@gmail.com>
2021-07-03 18:51:43 -0400

    further simplified code

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import styles from '../styles/Home.module.css'
-
 const Home = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>{`Counter: ${0}`}</h1>
-      <button className={styles.counterBtn}>Increment Counter</button>
+    <div className="container">
+      <h1>{`Counter: ${0}`}</h1>
+      <button className="counterBtn">Increment Counter</button>
     </div>
   )
 };
